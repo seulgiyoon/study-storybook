bca018b15ff01408a645b9eb0f4f20f936a3fc4b
Ellie <sgyoon.info@gmail.com>
2020-07-15 13:41:26 +0900

    Add page 컴포넌트

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './lib/redux';
 import PureTaskList from './components/PureTaskList';
+import InboxScreen from './components/InboxScreen';
 // import Option from './components/Option';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <PureTaskList />
+        <InboxScreen />
+        {/* <PureTaskList /> */}
         {/* <Option title="Hello Storybook" /> */}
       </Provider>
     );
